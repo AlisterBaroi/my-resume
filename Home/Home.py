ae764642232d5ca60f457f5586b2c0245ccf0e84
Alister Animesh Baroi <44337842+AlisterBaroi@users.noreply.github.com>
2024-06-13 15:29:05 +0600

    Create Home.py

---
+++ Home/Home.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Alister Animesh Baroi - Resume", page_icon=":page_facing_up:", layout="wide"
+)
+
+def main():
+  st.title("Receipts Demo")
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
