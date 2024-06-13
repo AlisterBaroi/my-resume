22d2265a42145bf646f56ea369d7d7320cdcfae6
Alister Animesh Baroi <44337842+AlisterBaroi@users.noreply.github.com>
2024-06-13 11:36:28 +0000

    init summary

--- Home.py
+++ Home.py
@@ -8,20 +8,28 @@ def main():
   row0 = st.columns([1,1])
 
   with row0[0]:
-     st.image("./images/Profile.png",width=360, caption="Holla")
+     st.image("./images/Profile.png",width=360)
      
   with row0[1]:
-    st.title("Alister Animesh Baroi")
+    st.header("Alister Animesh Baroi", anchor=False)
     st.write("Cloud Architect, DevOps Engineer, AI Engineer, Backend Engineer.")
     with open("./images/Alister_Baroi_Resume.pdf", "rb") as file:
-      st.download_button("Download Resume", type="primary", data=file, file_name="Alister_Baroi_Resume.pdf", mime="application/pdf")
+      st.download_button(":page_facing_up: Download Resume", type="primary", data=file, file_name="Alister_Baroi_Resume.pdf", mime="application/pdf")
 
   row1 = st.columns([1,1,1,1])
-  # git = st.image("https://static-00.iconduck.com/assets.00/github-icon-512x497-oppthre2.png")
-  row1[0].link_button(":globe_with_meridians: Website", url="https://alisterbaroi.streamlit.io", help="alisterbaroi.streamlit.io", use_container_width=True)
-  row1[1].link_button(":blue-background[in] LinkedIn", url="https://www.linkedin.com/in/alisterbaroi", help="linkedin.com/in/alisterbaroi", use_container_width=True)
-  row1[2].link_button(":cat: GitHub", url="https://github.com/alisterbaroi", help="github.com/alisterbaroi", use_container_width=True)
-  row1[3].link_button(":red[@] Email", url="mailto:alister.baroi@gmail.com", help="alister.baroi@gmail.com", use_container_width=True)
+  row1[0].link_button(":orange[@ Email]", url="mailto:alister.baroi@gmail.com", help="alister.baroi@gmail.com", use_container_width=True)
+  row1[1].link_button(":red[:globe_with_meridians: Website]", url="https://alisterbaroi.streamlit.io", help="alisterbaroi.streamlit.io", use_container_width=True)
+  row1[2].link_button(":blue-background[in] :blue[LinkedIn]", url="https://www.linkedin.com/in/alisterbaroi", help="linkedin.com/in/alisterbaroi", use_container_width=True)
+  row1[3].link_button(":computer: :green[GitHub]", url="https://github.com/alisterbaroi", help="github.com/alisterbaroi", use_container_width=True)
+
+  st.subheader("Summary", anchor=False)
+  st.markdown(""" 
+- 3+ years experience,
+- 👔 Lead Cloud Architect (current job),
+- 🏆 Computer Science (Honours) graduate from Taylors University (Malaysia),
+  - 🔥 Majored in AI & minored in FinTech,
+
+""")
 
 
 
