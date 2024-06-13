e17ec2102192ab7d1f1e3588ef612e4bbb99eada
Alister Animesh Baroi <44337842+AlisterBaroi@users.noreply.github.com>
2024-06-13 15:30:52 +0600

    Create Projects.py

---
+++ pages/Projects.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="My Projects", page_icon=":page_facing_up:", layout="wide"
+)
+
+def main():
+  st.title("My Projects")
+
+
+
+if __name__ == "__main__":
+    main()
