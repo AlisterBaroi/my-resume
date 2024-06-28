d595667b15507f0355dbe0e85e787ebea7129e66
Alister Animesh Baroi <44337842+AlisterBaroi@users.noreply.github.com>
2024-06-28 18:52:25 +0000

    trying ifreme

--- Home.py
+++ Home.py
@@ -265,7 +265,8 @@ def main():
 
   st.markdown("To see the full list of my achievement (from LinkedIn), [click here](https://www.linkedin.com/in/alisterbaroi/details/honors/)", unsafe_allow_html=True)
 
-  components.html("<meta name='google-site-verification' content='OP3yEmLoPHFKz6nzUVU_aWuso0ZWhv2MYBNlE0VQb0' />")
+  # components.html("<meta name='google-site-verification' content='OP3yEmLoPHFKz6nzUVU_aWuso0ZWhv2MYBNlE0VQb0' />")
+  components.iframe("<meta name='google-site-verification' content='OP3yEmLoPHFKz6nzUVU_aWuso0ZWhv2MYBNlE0VQb0' />")
 
 @st.experimental_dialog("Achievement", width="small")
 def achievements(item, item2, item3, item4, item5, item6):
