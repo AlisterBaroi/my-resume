5bec4fa33008f8a59799cc65701e04ba2c52c347
Alister Animesh Baroi <44337842+AlisterBaroi@users.noreply.github.com>
2024-06-28 18:36:37 +0000

    retry google verification

--- Home.py
+++ Home.py
@@ -265,7 +265,7 @@ def main():
 
   st.markdown("To see the full list of my achievement (from LinkedIn), [click here](https://www.linkedin.com/in/alisterbaroi/details/honors/)", unsafe_allow_html=True)
 
-  components.html(f'<meta name="google-site-verification" content="OP3yEmLoPHFKz6nzUVU_aWuso0ZWhv2MYBNlE0VQb0k" />')
+  components.html("""<meta name="google-site-verification" content="OP3yEmLoPHFKz6nzUVU_aWuso0ZWhv2MYBNlE0VQb0k" />""")
 
 
 @st.experimental_dialog("Achievement", width="small")
