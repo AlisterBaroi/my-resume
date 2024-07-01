04b2998aeb021c2af9376afff20551c2ae9c1edc
Alister Animesh Baroi <44337842+AlisterBaroi@users.noreply.github.com>
2024-07-01 16:13:53 +0000

    height none

--- Home.py
+++ Home.py
@@ -13,7 +13,7 @@ st._config.set_option('theme.base' ,"dark")
 # Include Google Analytics tracking code (doesnt work)
 with open("analytics.html", "r") as f:
     html_code = f.read()
-    components.html(html_code, height=0)
+    components.html(html_code, height=None)
   
 # components.iframe('analytics.html', height=1, scrolling=False)
 # components.html(r'analytics.html')
