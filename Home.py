a3a3c707539032a7995a7ebc2c4bd82fec24ba3a
Alister Animesh Baroi <44337842+AlisterBaroi@users.noreply.github.com>
2024-07-01 17:34:59 +0600

    Update Home.py

--- Home.py
+++ Home.py
@@ -3,6 +3,11 @@ from pathlib import Path
 import os
 import streamlit.components.v1 as components
 
+# Include Google Analytics tracking code
+with open("analytics.html", "r") as f:
+    html_code = f.read()
+    components.html(html_code, height=0)
+
 st.set_page_config(
     page_title="Alister Animesh Baroi - Portfolio Website", page_icon=":page_facing_up:", layout="centered")
 st._config.set_option('theme.base' ,"dark")
@@ -15,10 +20,7 @@ metadata = """<header>
 <p style="display: none;">Welcome to the website of Alister Animesh Baroi or Alister Baroi. This is Alisters online portfolio and digital resume website</p>"""
 st.markdown(metadata, unsafe_allow_html=True)
 
-# Include Google Analytics tracking code
-with open("analytics.html", "r") as f:
-    html_code = f.read()
-    components.html(html_code, height=0)
+
 
 # components.iframe('analytics.html', height=1, scrolling=False)
 # components.html(r'analytics.html')
