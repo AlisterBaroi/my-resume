39231be6d0a8d51983278063951df05ab660b413
Alister Animesh Baroi <44337842+AlisterBaroi@users.noreply.github.com>
2024-07-01 11:24:30 +0600

    Update 2_Contact.py

--- pages/2_Contact.py
+++ pages/2_Contact.py
@@ -39,8 +39,8 @@ def main():
      # components.html("""<script src='https://platform.linkedin.com/badges/js/profile.js' type='text/javascript'></script><div class="badge-base LI-profile-badge" data-locale="en_US" data-size="small" data-theme="light" data-type="VERTICAL" data-vanity="alisterbaroi" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://bd.linkedin.com/in/alisterbaroi?trk=profile-badge"></a></div>""", height=330)
      components.html("<script src='https://platform.linkedin.com/badges/js/profile.js' type='text/javascript'></script>", height=50)
      components.html("""<div class="badge-base LI-profile-badge" data-locale="en_US" data-size="small" data-theme="light" data-type="VERTICAL" data-vanity="alisterbaroi" data-version="v1">
-                             <a class="badge-base__link LI-simple-link" href="https://bd.linkedin.com/in/alisterbaroi?trk=profile-badge"></a>
-                          </div>""", height=330)
+     <a class="badge-base__link LI-simple-link" href="https://bd.linkedin.com/in/alisterbaroi?trk=profile-badge"></a>
+     </div>""", height=330)
 
   with form_sec[1]:
     with st.form(key="contact"):
