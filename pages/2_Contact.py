897079b6be1aa8761ef43570d44d1ed657049e2a
Alister Animesh Baroi <44337842+AlisterBaroi@users.noreply.github.com>
2024-07-01 18:33:27 +0600

    Update 2_Contact.py

--- pages/2_Contact.py
+++ pages/2_Contact.py
@@ -37,10 +37,14 @@ def main():
   form_sec = st.columns([5, 8])
   with form_sec[0]:
      # components.html("""<script src='https://platform.linkedin.com/badges/js/profile.js' type='text/javascript'></script><div class="badge-base LI-profile-badge" data-locale="en_US" data-size="small" data-theme="light" data-type="VERTICAL" data-vanity="alisterbaroi" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://bd.linkedin.com/in/alisterbaroi?trk=profile-badge"></a></div>""", height=330)
-     components.html("""<script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
-     <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="light" data-type="VERTICAL" data-vanity="alisterbaroi" data-version="v1">
-     <a class="badge-base__link LI-simple-link" href="https://bd.linkedin.com/in/alisterbaroi?trk=profile-badge"> Alister Animesh Baroi</a>
-     </div>""", height=345)
+     # components.html("""<script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
+     # <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="light" data-type="VERTICAL" data-vanity="alisterbaroi" data-version="v1">
+     # <a class="badge-base__link LI-simple-link" href="https://bd.linkedin.com/in/alisterbaroi?trk=profile-badge"> Alister Animesh Baroi</a>
+     # </div>""", height=345)
+      # Include Google Analytics tracking code (doesnt work)
+  with open("linkedin_embed.html", "r") as f:
+      html_code = f.read()
+      components.html(html_code, height=345)
 
   with form_sec[1]:
     with st.form(key="contact"):
